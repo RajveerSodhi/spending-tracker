147f6c530f6f585e6e98ad9d5a8f4402d36af69f
RajveerSodhi <rajveersodhi03@gmail.com>
2024-12-14 03:59:38 -0800

    revamped blocked page

--- background.js
+++ background.js
@@ -202,3 +202,13 @@ chrome.runtime.onMessage.addListener((message, sender) => {
         });
     }
 });
+
+// background.js
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+    if (message.action === "closeCurrentTab" && sender.tab) {
+      chrome.tabs.remove(sender.tab.id, () => {
+        console.log(`Closed tab with ID: ${sender.tab.id}`);
+      });
+    }
+  });
